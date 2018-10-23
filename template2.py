75df5687e86820290ae234bc0106d5f9c25ffa66
krilo <krilo_c@hotmail.com>
2018-10-23 14:28:34 +0800

    plotly

--- template2.py
+++ template2.py
@@ -1,2 +1,15 @@
 from flask import render_template
 from flask import Flask
+
+import plotly as py
+import plotly.graph_objs as go
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return None
+
+
+if __name__ == '__main__':
+    app.run()
